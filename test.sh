ba6592cde701fdb792bc95bdb9c38465fbec8072
huifenglan <huifeng1@vt.edu>
2017-10-31 21:10:55 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 touch summery
+#touch plo.dat
+#echo "# scenarios, max, min, aveg" >> plo.dat
 difference=0
 max=-1
 min=1
-summ=0
+sum=0
 aveg=0
 
 for i in 1 2 3 4 5 6 7 8 9
 do 
 	count=0
-	while read nob
+	while read boutfile
 	do
 		#echo "bout$i"
-		read datab
-		read temp
-		while read noh
+		read optimal
+		read emptyline
+		while read houtfile
 		do
 			#echo "hout$i"			
-			read datah
-			read temp
+			read heuristic
+			read emptyline
 			
 			if [ $nob == $noh ]; then
 				count=$(( count+1 ))
 				
-				diffrence=$(echo "$datah - $datab" | bc)
-				#echo $(bc<<<"$diffrence") >> summery
-				#echo "difference is $diffrence" >> summery	 					
-				summ=$(echo "$summ + $diffrence" | bc)
+				diffrence=$(echo "$heuristic - $optimal" | bc)					
+				sum=$(echo "$sum + $diffrence" | bc)
 
 				[ $(echo "$diffrence > $max" | bc) -eq 1 ] &&
 				max=$(echo "$diffrence" | bc)
@@ -40,10 +40,21 @@ do
 		
 	done < ./data/opt/bout$i
 	
-	aveg=$(echo "scale=10; $summ / $count" | bc)
+	aveg=$(echo "scale=10; $sum / $count" | bc)
 	echo "the data for scenario$i" >> summery
-	echo "The maximum of difference is $max" >> summery
-	echo "The minimum difference is $min" >>summery
-	echo "The average difference is $aveg" >>summery
+	echo "The maximum of difference: $max" >> summery
+	echo "The minimum difference: $min" >>summery
+	echo "The average difference: $aveg" >>summery
+	#echo "$i, $max, $min, $aveg" >> plo.dat
 done
 
+#gnuplot << EOF
+#set grid
+#set title 'Difference of Data'
+#set yrange [-1:1]
+#set xrange [0:10]
+#set xlabel 'scenarios'
+#set term png
+#set output "./graph.png"
+#plot 'plo.dat' u 1:2 w lp t 'maximum', 'plo.dat' u 1:3 w lp t 'minimum', 'plo.dat' u 1:4 w lp t 'average'
+#EOF
